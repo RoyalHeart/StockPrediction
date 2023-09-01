ada60a081c43319f710dedb71d1de28b4ba737f8
Tam Thai <hoangtam3062002@gmail.com>
2023-09-01 16:24:53 +0700

    Update web main

--- web/main.py
+++ web/main.py
@@ -7,15 +7,6 @@ from stock.stock import Stock
 
 app = FastAPI()
 app.mount("/static", StaticFiles(directory="static"), name="static")
-origins = ['http://localhost:3000','http://192.168.178.23:3000']
-
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=origins,
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
 templates = Jinja2Templates(directory="templates")
 
 stocks = [Stock("VIC"),Stock("ACB"),Stock("VJC")]
